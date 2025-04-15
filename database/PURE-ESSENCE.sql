b92c93e9cdf8d4fae47a60fef2e7f1754db624a0
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-15 18:57:57 +0200

    Added section to insert categories in database

--- database/PURE-ESSENCE.sql
+++ database/PURE-ESSENCE.sql
@@ -208,6 +208,35 @@ alter table `RICEZIONE-VENDITORE` add constraint FKRIC_VEN
      references VENDITORE (username);
 
 
--- Category creation - TODO
+-- Categories
 -- _____________ 
 
+insert into CATEGORIA(nome, descrizione)
+values (
+     "make-up",
+     "Il trucco è sempre stato e sempre sarà al centro della routine di bellezza della persona. Scopri ora il catalogo PureEssence e lasciati ispirare dalla nostra idea di make-up."
+);
+
+insert into CATEGORIA(nome, descrizione)
+values (
+     "viso",
+     "La formula della bellezza passa per la cura del viso. Scopri la linea PureEssence e trova i trattamenti e i prodotti migliori per curare e valorizzare il tuo viso."
+);
+
+insert into CATEGORIA(nome, descrizione)
+values (
+     "capelli",
+     "Che siano ricci, lisci, crespi o mossi, tutti cercano di trovare il miglior modo per valorizzare e curare i propri capelli. Scopri il catalogo PureEssence e trova il prodotto più giusto per te."
+);
+
+insert into CATEGORIA(nome, descrizione)
+values (
+     "corpo",
+     "Nella routine di bellezza quotidiana, nulla è importante come la cura della pelle. Scopri la collezione di prodotti corpo PureEssence e trova tutto ciò di cui hai bisogno per una pelle perfetta."
+);
+
+insert into CATEGORIA(nome, descrizione)
+values (
+     "profumi",
+     "Che sia per un'occasione speciale, o per la tua vita quotidiana, PureEssence ha il profumo giusto per te. Scopri il nostro catalogo e trova la migliore fragranza per le tue esigenze."
+);
