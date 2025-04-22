ae0f5931b9c379fd73c9ff7d79e1968b9ef5ddb6
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-22 19:31:10 +0200

    Added php base page template

---
+++ sito/templates/base.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title> <?php echo $templateParams["titolo"]; ?> </title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="style/style.css"/>
+    </head>
+
+    <body>
+        <header>
+            <nav><a href="#"><img src="<?php echo LOCAL_IMG_DIR."list.svg"; ?>" alt="Categorie" /></a>
+            </nav><div><a href="index.php"><img src="<?php echo LOCAL_IMG_DIR."logo.png"; ?> " alt="" /></a>
+            </div><nav><ul>
+                <li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."search.svg"; ?>" alt="Cerca" /></a>
+                </li><li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."heart.svg"; ?>" alt="Preferiti" /></a>
+                </li><li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."person.svg"; ?>" alt="Profilo" /></a>
+                </li><li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."handbag.svg"; ?>" alt="Carrello" /></a></li>
+        </ul></nav>
+        </header>
+
+        <nav>
+            <ul>
+                <?php foreach($templateParams["categorie"] as $categoria): ?><li><a href="#"><?php echo strtoupper($categoria["nome"]) ?></a></li><?php endforeach; ?>
+            </ul>
+        </nav>
+
+        <main>
+            <?php
+                if (isset($templateParams["main"])) {
+                    require($templateParams["main"]);
+                }
+            ?>
+        </main>
+
+        <footer>
+            <section>
+                <header><h2>Politiche aziendali</h2></header>
+                <nav><ul>
+                    <li><a href="#">Informativa sulla privacy</a></li>
+                    <li><a href="#">Spedizioni e resi</a></li>
+                    <li><a href="#">Metodi di pagamento</a></li>
+                </ul></nav>
+            </section><section>
+                <header><h2>I nostri social</h2></header>
+                <nav><ul>
+                    <li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."instagram-logo.png"; ?>" alt="Instagram" /></a>
+                    </li><li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."facebook-logo.png"; ?>" alt="Facebook" /></a>
+                    </li><li><a href="#"><img src="<?php echo LOCAL_IMG_DIR."x-logo.png"; ?>" alt="X o Twitter" /></a></li>
+                </ul></nav>
+            </section>
+            <p>© 2025 PureEssence</p>
+        </footer>
+    </body>
+</html>
