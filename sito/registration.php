53e9160696c156ba39e01adb0e5c2c70b5f33c24
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-23 13:03:31 +0200

    Added handling of possible registration errors

--- sito/registration.php
+++ sito/registration.php
@@ -9,6 +9,16 @@ if (isset($_SESSION["idutente"])) {
 
     $templateParams["main"] = "templates/form.php";
     $templateParams["tipoForm"] = 1;
+
+    if (isset($_GET["err"])) {
+        if ($_GET["err"] == 1) {
+            $templateParams["erroreLogin"] = "Inserisci username e password per completare la registrazione.";
+        } else if ($_GET["err"] == 2) {
+            $templateParams["erroreLogin"] = "Le due password non coincidono.";
+        } else if ($_GET["err"] == 3) {
+            $templateParams["erroreLogin"] = "Le due email non coincidono.";
+        }
+    }
 }
 
 require 'templates/base.php';
