e2466d8ed646d075568187c284a61ce623077bdf
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-23 16:41:57 +0200

    Added login error handling to display proper messages

--- sito/login.php
+++ sito/login.php
@@ -9,6 +9,14 @@ if (isset($_SESSION["idutente"])) {
 
     $templateParams["main"] = "templates/form.php";
     $templateParams["tipoForm"] = 0;
+
+    if (isset($_GET["err"])) {
+        if ($_GET["err"] == 1) {
+            $templateParams["erroreLogin"] = "Inserire uno username e una password.";
+        } else if ($_GET["err"] == 2) {
+            $templateParams["erroreLogin"] = "Username o password errati.";
+        }
+    }
 }
 
 require 'templates/base.php';
