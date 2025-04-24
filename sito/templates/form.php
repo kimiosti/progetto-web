8b1eb47439b42d7832f9133d368b1c0173ba63ef
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-24 22:19:42 +0200

    Modified code to extend form template use cases

--- sito/templates/form.php
+++ sito/templates/form.php
@@ -2,7 +2,7 @@
 <section>
     <form action="profile.php" method="post">
         <h1><?php
-            if ($templateParams["tipoForm"] == 1) {
+            if ($templateParams["tipoForm"] == "registrazione") {
                 echo "Registrazione";
             } else {
                 echo "Login";
@@ -14,47 +14,92 @@
             }
         ?></p>
         <ul>
-            <li>
-                <label for="username">Username:</label><input type="text" id="username" name="username" />
-            </li>
             <?php
-                if ($templateParams["tipoForm"] == 1) {
+                if ($templateParams["tipoForm"] == "registrazione") {
                     echo <<<EOD
-                    <li>
-                        <label for="email">Email:</label><input type="email" id="email" name="email" />
-                    </li>
-                    <li>
-                        <label for="confermaEmail">Ripeti email:</label><input type="email" id="confermaEmail" name="confermaEmail" />
-                    </li>
+                        <li>
+                            <label for="username">Username:</label><input type="text" id="username" name="username" />
+                        </li>
+                        <li>
+                            <label for="password">Password:</label><input type="password" id="password" name="password" />
+                        </li>
+                        <li>
+                            <label for="confermaPassword">Ripeti password:</label><input type="password" id="confermaPassword" name="confermaPassword" />
+                        </li>
+                        <li>
+                            <label for="email">Email:</label><input type="email" id="email" name="email" />
+                        </li>
+                        <li>
+                            <label for="confermaEmail">Ripeti email:</label><input type="email" id="confermaEmail" name="confermaEmail" />
+                        </li>
+                        <li>
+                            <label for="telefono">Telefono:</label><input type="tel" id="telefono" name="telefono" maxlength="10" />
+                        </li>
+                        <li>
+                            <label for="confermaTelefono">Ripeti telefono:</label><input type="tel" id="confermaTelefono" name="confermaTelefono" maxlength="10" />
+                        </li>
+                        <li>
+                            <input type="submit" name="submit" value="Registrati" />
+                        </li>
+                    EOD;
+                } else if ($templateParams["tipoForm"] == "cambiaEmail") {
+                    echo <<<EOD
+                        <li>
+                            <label for="email">Email:</label><input type="email" id="email" name="email" />
+                        </li>
+                        <li>
+                            <label for="confermaEmail">Ripeti email:</label><input type="email" id="confermaEmail" name="confermaEmail" />
+                        </li>
+                        <li>
+                            <input type="submit" name="submit" value="Cambia email" />
+                        </li>
+                    EOD;
+                } else if ($templateParams["tipoForm"] == "cambiaPass") {
+                    echo <<<EOD
+                        <li>
+                            <label for="vecchiaPassword">Vecchia password:</label><input type="password" id="vecchiaPassword" name="password" />
+                        </li>
+                        <li>
+                            <label for="password">Nuova password:</label><input type="password" id="password" name="password" />
+                        </li>
+                        <li>
+                            <label for="confermaPassword">Ripeti password:</label><input type="password" id="confermaPassword" name="confermaPassword" />
+                        </li>
+                        <li>
+                            <input type="submit" name="submit" value="Cambia password" />
+                        </li>
+                    EOD;
+                } else if ($templateParams["tipoForm"] == "cambiaTelefono") {
+                    echo <<<EOD
+                        <li>
+                            <label for="telefono">Telefono:</label><input type="tel" id="telefono" name="telefono" maxlength="10" />
+                        </li>
+                        <li>
+                            <label for="confermaTelefono">Ripeti telefono:</label><input type="tel" id="confermaTelefono" name="confermaTelefono" maxlength="10" />
+                        </li>
+                        <li>
+                            <input type="submit" name="submit" value="Cambia telefono" />
+                        </li>
+                    EOD;
+                } else {
+                    echo <<<EOD
+                        <li>
+                            <label for="username">Username:</label><input type="text" id="username" name="username" />
+                        </li>
+                        <li>
+                            <label for="password">Password:</label><input type="password" id="password" name="password" />
+                        </li>
+                        <li>
+                            <input type="submit" name="submit" value="Accedi" />
+                        </li>
                     EOD;
                 }
             ?>
-            <li>
-                <label for="password">Password:</label><input type="password" id="password" name="password" />
-            </li>
-            <?php
-                if ($templateParams["tipoForm"] == 1) {
-                    echo <<<EOD
-                    <li>
-                        <label for="confermaPassword">Ripeti password:</label><input type="password" id="confermaPassword" name="confermaPassword" />
-                    </li>
-                    EOD;
-                }
-            ?>
-            <li>
-                <input type="submit" name="submit" value="<?php
-                    if ($templateParams["tipoForm"] == 1) {
-                        echo "Registrati";
-                    } else {
-                        echo "Accedi";
-                    }
-                ?>" />
-            </li>
         </ul>
         <p><?php
-            if ($templateParams["tipoForm"] == 1) {
+            if ($templateParams["tipoForm"] == "registrazione") {
                 echo '<a href="login.php">Hai già un account? <span>Accedi!</span></a>';
-            } else {
+            } else if ($templateParams["tipoForm"] == "login") {
                 echo '<a href="registration.php">Non hai ancora un account? <span>Registrati!</span></a>';
             }
         ?></p>
