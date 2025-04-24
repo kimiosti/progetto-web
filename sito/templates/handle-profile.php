8a83c5dd788d75787a258d9144c643518df10f7f
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-24 17:37:50 +0200

    Completed layout for profile handling page

--- sito/templates/handle-profile.php
+++ sito/templates/handle-profile.php
@@ -1,15 +1,35 @@
-<link rel="stylesheet" type="text/css" href="style/handle-profile.css"/>
+<link rel="stylesheet" type="text/css" href="style/profile.css"/>
 <?php 
 $email = "";
+$telefono = "";
 if ($_SESSION["tipoUtente"] == 1) {
-    $email = $dbh->getCustomerEmail($_SESSION["idutente"])[0]["email"];
+    $email = $dbh->getCustomerInfo($_SESSION["idutente"])[0]["email"];
+    $telefono = $dbh->getCustomerInfo($_SESSION["idutente"])[0]["telefono"];
 } else {
-    $email = $dbh->getSellerEmail($_SESSION["idutente"])[0]["email"];
+    $email = $dbh->getSellerInfo($_SESSION["idutente"])[0]["email"];
+    $telefono = $dbh->getSellerInfo($_SESSION["idutente"])[0]["telefono"];
 }
 ?>
 <section>
     <h1>Anagrafica del profilo</h1>
-    <p>La tua email: <span><?php echo $email; ?></span></p>
-    <nav><a href="#"><button>Cambia email</button></a>
-    <a href="#"><button>Cambia password</button></a></nav>
+    <?php
+        if ($email == "") {
+            echo '<p class="errore">Non hai ancora registrato un indirizzo email.</p>';
+        } else {
+            echo '<p>La tua email: <span>' . $email . '</span></p>';
+        }
+
+        if ($telefono == "") {
+            echo '<p class="errore">Non hai ancora registrato un numero di telefono.</p>';
+        } else {
+            echo '<p>Il tuo numero di telefono: <span>' . $telefono . '</span></p>';
+        }
+    ?>
+    <nav><ul>  
+        <li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."envelope.svg"; ?>" alt="Cambia email" /><h2>Cambia email</h2></div></a>
+        </li><li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."key.svg"; ?>" alt="Cambia password" /><h2>Cambia password</h2></div></a>
+        </li><li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."telephone.svg"; ?>" alt="Cambia telefono" /><h2>Cambia telefono</h2></div></a>
+        </li><li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."x.svg"; ?>" alt="Rimuovi account" /><h2>Rimuovi account<h2></div></a>
+    </ul></nav>
+    <a href="profile.php"><button>Torna indietro</button></a>
 </section>
