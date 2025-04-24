c44c25f14fea4a4d59d37f8bf1cff260561ec49c
Giulia <giulia.albertini3@studio.unibo.it>
2025-04-25 00:54:22 +0200

    Added category description parameter

--- sito/prodotti_categoria.php
+++ sito/prodotti_categoria.php
@@ -4,10 +4,10 @@ require_once 'setup.php';
 
 // Recupera la categoria selezionata tramite GET
 $categoria = $_GET["categoria"]; // recupera categoria dal parametro GET
-
 // Imposta il titolo, carica le categorie e i prodotti per quella categoria
 $templateParams["titolo"] = "PureEssence - Prodotti $categoria";
 $templateParams["categorie"] = $dbh->getCategories();
+$templateParams["descrizioneCategoria"] = $dbh->getDescriptionByCategories($categoria);
 $templateParams["prodotti"] = $dbh->getProductByCategories($categoria); // Recupera i prodotti per quella categoria
 $templateParams["categoriaSelezionata"] = $categoria; // Passa la categoria selezionata per visualizzarla nel template
 $templateParams["main"] = "prodotti_categoria_contenuto.php"; // Il contenuto da caricare
