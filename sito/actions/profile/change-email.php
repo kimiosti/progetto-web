2c6bbc529c0e6ea1df59970d1ac3421b327408b2
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-25 10:39:44 +0200

    Added check to ensure user is logged before loading page

--- sito/actions/profile/change-email.php
+++ sito/actions/profile/change-email.php
@@ -1,17 +1,22 @@
 <?php
 require_once '../../setup.php';
-if(!isset($_POST["email"]) || !isset($_POST["confermaEmail"])) {
-    $_SESSION["mess"] = "Formato richiesta non supportato.";
-} else if (strcmp($_POST["email"], $_POST["confermaEmail"]) != 0) {
-    $_SESSION["mess"] = "Le due email non coincidono.";
-} else if (empty($_POST["email"])) {
-    $_SESSION["mess"] = "Inserire una mail.";
-} else {
-    if ($dbh->setEmail($_SESSION["tipoUtente"], $_SESSION["idutente"], $_POST["email"])) {
-        $_SESSION["mess"] = "Email cambiata correttamente";
+if (isset($_SESSION["idutente"]) && isset($_SESSION["tipoUtente"])) {
+    if(!isset($_POST["email"]) || !isset($_POST["confermaEmail"])) {
+        $_SESSION["mess"] = "Formato richiesta non supportato.";
+    } else if (strcmp($_POST["email"], $_POST["confermaEmail"]) != 0) {
+        $_SESSION["mess"] = "Le due email non coincidono.";
+    } else if (empty($_POST["email"])) {
+        $_SESSION["mess"] = "Inserire una mail.";
     } else {
-        $_SESSION["mess"] = "Errore nella modifica della mail.";
+        if ($dbh->setEmail($_SESSION["tipoUtente"], $_SESSION["idutente"], $_POST["email"])) {
+            $_SESSION["mess"] = "Email cambiata correttamente";
+        } else {
+            $_SESSION["mess"] = "Errore nella modifica della mail.";
+        }
     }
+} else {
+    header("Location: ../../login.php");
+    die();
 }
 
 header("Location: ../../set-email.php");
