9009d7f97850bacf774c8aa4e4d932a3bd533307
Giulia <giulia.albertini3@studio.unibo.it>
2025-04-25 16:51:49 +0200

    Added params

--- sito/prodotti_categoria.php
+++ sito/prodotti_categoria.php
@@ -4,6 +4,10 @@ require_once 'setup.php';
 
 // Recupera la categoria selezionata tramite GET
 $categoria = $_GET["categoria"]; // recupera categoria dal parametro GET
+
+$brandList = $dbh->getBrand($categoria); // Supponendo che $dbHandler sia il tuo oggetto DB
+$subcategoryList = $dbh->getSubcategory($categoria); // Supponendo che $dbHandler sia il tuo oggetto DB
+
 // Imposta il titolo, carica le categorie e i prodotti per quella categoria
 $templateParams["titolo"] = "PureEssence - Prodotti $categoria";
 $templateParams["categorie"] = $dbh->getCategories();
@@ -12,6 +16,8 @@ $templateParams["prodotti"] = $dbh->getProductByCategories($categoria); // Recup
 $templateParams["categoriaSelezionata"] = $categoria; // Passa la categoria selezionata per visualizzarla nel template
 $templateParams["main"] = "prodotti_categoria_contenuto.php"; // Il contenuto da caricare
 $templateParams["css"] = "style/prodotti_categoria.css";  // Aggiungi il CSS specifico per questa pagina
+$templateParams["marcafiltri"]= $brandList;
+$templateParams["sottocategoriafiltri"]= $subcategoryList;
 
 require 'templates/base.php';
 ?>
