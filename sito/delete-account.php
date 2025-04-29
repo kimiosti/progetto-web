e32d29f9a774511f33a18b962b7ee62eba821e4b
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-29 23:26:09 +0200

    Modified page to handle deletion errors

--- sito/delete-account.php
+++ sito/delete-account.php
@@ -6,7 +6,13 @@ $templateParams["categorie"] = $dbh->getCategories();
 
 $templateParams["main"] = "templates/form.php";
 $templateParams["tipoForm"] = "rimuoviAccount";
-$templateParams["messaggioForm"] = "Clicca per confermare la rimozione dell'account.<br />L'azione non potrà essere annullata.";
+
+if (isset($_SESSION["erroreRimozione"])) {
+    $templateParams["messaggioForm"] = $_SESSION["erroreRimozione"];
+    unset($_SESSION["erroreRimozione"]);
+} else {
+    $templateParams["messaggioForm"] = "Clicca per confermare la rimozione dell'account.<br />L'azione non potrà essere annullata.";
+}
 
 require 'templates/base.php';
 ?>
