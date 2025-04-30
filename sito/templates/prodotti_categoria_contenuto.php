a9454996e1d6ba225dab2c017965de5f25e969e3
Giulia <giulia.albertini3@studio.unibo.it>
2025-04-30 17:24:05 +0200

    Added breadcrumb

--- sito/templates/prodotti_categoria_contenuto.php
+++ sito/templates/prodotti_categoria_contenuto.php
@@ -1,5 +1,6 @@
 <!-- pagina prodotti categoria -->
 <link rel="stylesheet" type="text/css" href="style/stylepage.css"/>
+<script src="script/filterpage.js"></script>
 <div class="prodotti_categoria">
     <h2><?php echo strtoupper(htmlspecialchars($templateParams['categoriaSelezionata'])); ?></h2>
     <div class="intro">
@@ -7,24 +8,46 @@
         <h3><?php echo htmlspecialchars($templateParams["descrizioneCategoria"]); ?> </h3>
     </div>
 
-    <div class= "filtri">
-        <button class="click">Marca<img class="arrow" src="img/arrow-up.png"></button>
-        <ul class="dropdown">
-            <?php foreach($templateParams['marcafiltri'] as $marca): ?>
-                <li><h3><?php echo htmlspecialchars($marca['marca']); ?></h3></li>
-            <?php endforeach; ?>
-        </ul>
-        <button class="click">Taglia<img class="arrow" src="img/arrow-up.png"></button>
-        <ul class="dropdown">
-            <?php foreach($templateParams['sottocategoriafiltri'] as $sottocategoria): ?>
-                <li><h3><?php echo htmlspecialchars($sottocategoria['sottocategoria']); ?></h3></li>
-            <?php endforeach; ?>
-        </ul>
-        <button>Tipo di prodotto<img src="img/arrow-up.png"></button>
-        <button>Prezzo<img src="img/arrow-up.png"></button>
-    </div>
+<!-- percorso -->
+    <nav class="breadcrumb"> 
+        <a href="index.php">Home</a> /
+        <span><?php echo ucfirst(htmlspecialchars($templateParams['categoriaSelezionata'])); ?></span>
+        <h3 class=titoletto>Filtri</h3>
+    </nav>
 
     
+
+<!-- menù a tendina filtri -->
+    <div class= "filtri">
+        <div class="wrapper">
+            <button class="click" >Marca<img class="arrow" src="img/arrow-up.png"></button>
+                <ul class="dropdown">
+                    <?php foreach($templateParams['marcafiltri'] as $marca): ?>
+                        <li><p><?php echo ucfirst(htmlspecialchars($marca['marca']));?></p></li>
+                    <?php endforeach; ?>
+                </ul>
+        </div>
+    
+        <div class="wrapper">
+            <button class="click">Tipologia di prodotto<img class="arrow" src="img/arrow-up.png"></button>
+                <ul class="dropdown">
+                    <?php foreach($templateParams['sottocategoriafiltri'] as $sottocategoria): ?>
+                        <li><p><?php echo ucfirst(htmlspecialchars($sottocategoria['sottocategoria']));?></p></li>
+                    <?php endforeach; ?>
+                </ul>
+        </div>
+    
+        <div class="wrapper">
+            <button>Taglia<img src="img/arrow-up.png"></button>
+        </div>
+
+        <div class="wrapper">
+            <button>Prezzo<img src="img/arrow-up.png"></button>
+        </div>
+
+    </div>
+
+<!-- prodotti -->
     <div class="elenco_prodotti">
         <?php
         if (empty($templateParams["prodotti"])) {
@@ -41,7 +64,4 @@
         }
         ?>
     </div>
-
-       
-
 </div>
