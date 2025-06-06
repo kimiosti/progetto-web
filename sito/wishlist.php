dc7ebd701860269167c38ec12b6c80774d1b25dc
Kimi Osti <kimi.osti@studio.unibo.it>
2025-06-06 12:07:09 +0200

    Added handling for seller trying to see wishlist

--- sito/wishlist.php
+++ sito/wishlist.php
@@ -3,6 +3,19 @@ require_once 'setup.php';
 
 if (!isset($_SESSION["idutente"])) {
     header("Location: login.php");
+}else {
+    $templateParams["titolo"] = "PureEssence - Wishlist";
+    $templateParams["categorie"] = $dbh->getCategories();
+
+    if ($_SESSION["tipoUtente"] == "venditore") {
+        $templateParams["titoloMessaggio"] = "Wishlist non disponibile";
+        $templateParams["corpoMessaggio"] = "La Wishlist non può essere visualizzata dal venditore.";
+
+        $templateParams["main"] = "templates/message.php";
+    } else {
+        echo "acquirente";
+        $templateParams["main"] = null;
+    }
 }
 
 require 'templates/base.php';
