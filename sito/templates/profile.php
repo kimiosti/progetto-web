dcb7bac0a5b762640a23d8add497a6218d1af962
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-13 16:42:25 +0200

    Added link to availability handling page

--- sito/templates/profile.php
+++ sito/templates/profile.php
@@ -9,7 +9,7 @@
                 if (isset($_SESSION["tipoUtente"]) && $_SESSION["tipoUtente"] == "acquirente") {
                     echo '<li><a href="wishlist.php"><div><img src="'.LOCAL_IMG_DIR.'heart.svg'.'" alt="Lista dei desideri" /><h2>Lista dei desideri</h2></div>';
                 } else {
-                    echo '<li><a href="#"><div><img src="'.LOCAL_IMG_DIR.'plus.svg'.'" alt="Disponibilità" /><h2>Gestisci disponibilità</h2></div>';
+                    echo '<li><a href="availability.php"><div><img src="'.LOCAL_IMG_DIR.'plus.svg'.'" alt="Disponibilità" /><h2>Gestisci disponibilità</h2></div>';
                 }
             ?></a></li><li><a href="handle-profile.php"><div><img src="<?php echo LOCAL_IMG_DIR."pencil.svg"; ?>" alt="Anagrafica" /><h2>Gestisci il tuo profilo</h2></div></a></li>
         </ul>
