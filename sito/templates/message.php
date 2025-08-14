6312f8228eb22f04c67747a283b0e75824a0024e
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-14 13:55:29 +0200

    Modified message template to add go back button

--- sito/templates/message.php
+++ sito/templates/message.php
@@ -1,4 +1,5 @@
 <section>
     <header><h1><?php echo $templateParams["titoloMessaggio"]; ?></h1></header>
     <p><?php echo $templateParams["corpoMessaggio"]; ?></p>
+    <a href="<?php echo $templateParams["linkBottone"]; ?>"><button><?php echo $templateParams["testoBottone"]; ?></button></a>
 </section>
