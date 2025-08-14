4a4d1f33ce1133fdec13cdfa3fb9212545f473b2
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-14 13:51:46 +0200

    Modified availability page to display message on permission denial

--- sito/availability.php
+++ sito/availability.php
@@ -1,8 +1,16 @@
 <?php
 require_once 'setup.php';
 
-if (!isset($_SESSION["idutente"]) || $_SESSION["tipoUtente"] == "acquirente") {
+if (!isset($_SESSION["idutente"])) {
     header("Location: login.php");
+} else if ($_SESSION["tipoUtente"] != "venditore") {
+    $templateParams["titolo"] = "PureEssence - Accesso negato";
+    $templateParams["categorie"] = $dbh->getCategories();
+    $templateParams["titoloMessaggio"] = "Accesso negato";
+    $templateParams["corpoMessaggio"] = "Solamente i venditori possono gestire la disponibilità dei prodotti.";
+
+    $templateParams["main"] = "templates/message.php";
+    require 'templates/base.php';
 } else {
     $templateParams["titolo"] = "PureEssence - Disponibilità";
     $templateParams["categorie"] = $dbh->getCategories();
