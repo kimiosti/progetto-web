ad027dee0a5c8b68f910187f90bd0af4d7626152
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-17 12:48:32 +0200

    Modified default image folders

--- sito/setup.php
+++ sito/setup.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-define("LOCAL_IMG_DIR", "./img/");
-define("UPLOAD_DIR", "./upload-img/");
+define("LOCAL_IMG_DIR", "img/");
+define("UPLOAD_DIR", "img/uploads/");
 define("PRODUCT_CAPTION_DEFAULT", "Inserire qui il testo da visualizzare nell'anteprima del prodotto.");
 define("PRODUCT_DESCRIPTION_DEFAULT", "Inserire qui il testo da visualizzare nella pagina di dettaglio del prodotto.");
 define("PERMISSION_DENIED_PAGE_TITLE", "PureEssence - Accesso negato");
