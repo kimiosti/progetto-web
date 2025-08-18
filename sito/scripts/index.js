da5a58f0d04925c8ba516daf24389d319a8ba808
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-18 12:44:18 +0200

    Added base script to periodically check for notifications and update the notification icon accordingly

---
+++ sito/scripts/index.js
@@ -0,0 +1,17 @@
+function refresh() {
+    fetch("actions/profile/check-notifications.php", {
+        method: "POST"
+    }).then(res => {
+        return res.json();
+    }).then(data => {
+        element = document.querySelector("body > header > nav > ul > li > a > img[alt=Notifiche]");
+        if (data.hasNotifications == true) {
+            element.src = data.baseImageFolder + "bell-fill.svg";
+        } else {
+            element.src = data.baseImageFolder + "bell.svg";
+        }
+    });
+}
+
+refresh();
+setInterval(refresh, 2000);
