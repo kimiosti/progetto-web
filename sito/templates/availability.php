1973700de05227cfe183bae237e0b5457c8938d3
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-18 09:39:55 +0200

    Fixed html syntax errors

--- sito/templates/availability.php
+++ sito/templates/availability.php
@@ -1,11 +1,11 @@
-<link rel="stylesheet" href="style/multioption.css" type="text/css">
+<link rel="stylesheet" href="style/multioption.css" type="text/css" />
 <section>
     <header><h1>Gestione disponibilità</h1></header>
     <nav>
         <ul>
-            <li><a href="add-product.php"><div><img src="<?php echo LOCAL_IMG_DIR."plus.svg"; ?>" alt="Aggiungi prodotto" /><h2>Aggiungi prodotto</h2></div>
-            <li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."pencil.svg"; ?>" alt="Aggiorna disponibilità" /><h2>Aggiorna disponibilità</h2></div>
-        </ul>
+            <li><a href="add-product.php"><div><img src="<?php echo LOCAL_IMG_DIR."plus.svg"; ?>" alt="Aggiungi prodotto" /><h2>Aggiungi prodotto</h2></div></a>
+        </li><li><a href="#"><div><img src="<?php echo LOCAL_IMG_DIR."pencil.svg"; ?>" alt="Aggiorna disponibilità" /><h2>Aggiorna disponibilità</h2></div></a>
+    </li></ul>
     </nav>
     <a href="profile.php"><button>Torna indietro</button></a>
-<section>
+</section>
