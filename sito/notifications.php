e0b34948285ea159fc3790fec43242b04e805e0e
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-19 11:22:12 +0200

    Modified notifications page to conform to modified getter query

--- sito/notifications.php
+++ sito/notifications.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["idutente"])) {
     $templateParams["titolo"] = "PureEssence - Notifiche";
     $templateParams["categorie"] = $dbh->getCategories();
 
-    $templateParams["notifiche"] = $dbh->getAllNotifications($_SESSION["idutente"], $_SESSION["tipoUtente"], true);
+    $templateParams["notifiche"] = $dbh->getAllNotifications($_SESSION["idutente"], $_SESSION["tipoUtente"], false);
     $templateParams["main"] = 'templates/notifications.php';
 
     require_once 'templates/base.php';
