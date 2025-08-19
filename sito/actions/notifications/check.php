b9b80b332aceb4c88add2b58ceb9e1e7fd9a8cb7
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-19 10:08:55 +0200

    Moved and renamed api php file, and added header indicating response content type

--- sito/actions/notifications/check.php
+++ sito/actions/notifications/check.php
@@ -12,6 +12,7 @@ if (isset($_SESSION["idutente"])) {
     $obj->hasNotifications = $dbh->hasUnreadNotifications($_SESSION["idutente"], $_SESSION["tipoUtente"]);
 }
 
+header("Content-Type: application/json");
 echo json_encode($obj);
 
 ?>
