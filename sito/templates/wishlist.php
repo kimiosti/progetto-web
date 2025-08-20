738793859e9f0f3f03f7874dce84a38eba3c8e18
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-20 10:50:26 +0200

    Revised template structure to show wishlist products

--- sito/templates/wishlist.php
+++ sito/templates/wishlist.php
@@ -1,22 +1,15 @@
 <link rel="stylesheet" type="text/css" href="style/wishlist.css" />
 <section>
-<nav><a href="index.php">Home</a><span>/</span><a href="profile.php">Pagina personale</a><span>/Lista dei desideri</a></nav>
+<nav><a href="index.php">Home</a><span>/</span><a href="profile.php">Pagina personale</a><span>/Lista dei desideri</span></nav>
     <header><h1><?php echo $templateParams["titoloPagina"]; ?></h1></header>
     <p class="errore"><?php
         if (isset($templateParams["info"])) {
             echo $templateParams["info"];
         }
     ?></p>
-    <div class="prodotti_wishlist"><div class="elenco_prodotti">
-        <?php
-            foreach ($templateParams["prodotti"] as $prodotto) {
-                echo "<div class='prodotto'>";
-                echo "<h3>" . htmlspecialchars($prodotto['marca']) . "</h3>";
-                echo "<img src='img/" . htmlspecialchars($prodotto['URLimmagine']) . "' alt='" . htmlspecialchars($prodotto['nome']) . "' />";
-                echo "<h3>" . htmlspecialchars($prodotto['nome']) . "</h3>";
-                echo "<p>" . htmlspecialchars($prodotto['didascalia']) . "</p>";
-                echo "</div>";
-            }
-        ?>
-    </div></div>
+    <section><?php foreach ($templateParams["prodotti"] as $prodotto): ?><a href="product_detail.php?id=<?php echo $prodotto["IDprodotto"]; ?>"><div>
+                <header><h2><?php echo htmlspecialchars($prodotto["marca"]); ?></h2></header>
+                <div><img src="<?php echo $prodotto["URLimmagine"]; ?>" alt="<?php echo htmlspecialchars($prodotto["nome"]); ?>" /></div>
+                <section><h3><?php echo htmlspecialchars($prodotto["nome"]); ?></h3><p><?php echo htmlspecialchars($prodotto["didascalia"]); ?></p></section>
+            </div></a><?php endforeach; ?></section>
 </section>
