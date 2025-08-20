f6e4ae96937e41c760dd032c94bc0f71223aee18
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-20 11:36:39 +0200

    Modified wishlist template to display product price, if available

--- sito/templates/wishlist.php
+++ sito/templates/wishlist.php
@@ -11,5 +11,13 @@
                 <header><h2><?php echo htmlspecialchars($prodotto["marca"]); ?></h2></header>
                 <div><img src="<?php echo $prodotto["URLimmagine"]; ?>" alt="<?php echo htmlspecialchars($prodotto["nome"]); ?>" /></div>
                 <section><h3><?php echo htmlspecialchars($prodotto["nome"]); ?></h3><p><?php echo htmlspecialchars($prodotto["didascalia"]); ?></p></section>
+                <footer><?php
+                    if ($prodotto["prezzo"] == -1) {
+                        echo '<p class="errore">Non disponibile</p>';
+                    } else {
+                        echo '<h4>' . htmlspecialchars($prodotto["prezzo"]) . '€</h4>';
+                        echo '<p>' . htmlspecialchars($prodotto["taglia"]) . '</p>';
+                    }
+                ?></footer>
             </div></a><?php endforeach; ?></section>
 </section>
