daefdd6fdd508f8bb09006ff5c574584cf1ce6c3
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-22 22:32:58 +0200

    Defined constants for better product form values handling

--- sito/setup.php
+++ sito/setup.php
@@ -2,8 +2,11 @@
 session_start();
 define("LOCAL_IMG_DIR", "img/");
 define("UPLOAD_DIR", "img/uploads/");
-define("PRODUCT_CAPTION_DEFAULT", "Inserire qui il testo da visualizzare nell'anteprima del prodotto.");
-define("PRODUCT_DESCRIPTION_DEFAULT", "Inserire qui il testo da visualizzare nella pagina di dettaglio del prodotto.");
+define("PRODUCT_CAPTION_DEFAULT", "Inserire il testo da visualizzare nell'anteprima del prodotto.");
+define("PRODUCT_DESCRIPTION_DEFAULT", "Inserire il testo da visualizzare nella pagina di dettaglio del prodotto.");
+define("PRODUCT_INSTRUCTIONS_DEFAULT", "Inserire le istruzioni per l'uso.");
+define("PRODUCT_INGREDIENTS_DEFAULT", "Inserire gli ingredienti del prodotto.");
+define("PRODUCT_WARNINGS_DEFAULT", "Inserire le avvertenze di sicurezza.");
 define("PERMISSION_DENIED_PAGE_TITLE", "PureEssence - Accesso negato");
 define("PERMISSION_DENIED_TITLE", "Accesso negato");
 define("PERMISSION_DENIED_MESSAGE", "Gli account di questo tipo non possono effettuare l'azione richiesta.");
