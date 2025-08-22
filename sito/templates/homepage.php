7d3fd659b476abe427c0e6175ea7f045aae5aba7
Giulia <giulia.albertini3@studio.unibo.it>
2025-08-22 17:52:43 +0200

    Added homepage banner section with image, text, and button

---
+++ sito/templates/homepage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="style/home.css"/>
+<div class="homepage-banner">
+    <img src="img/homepage.jpg" alt="Banner image" class="banner-img">
+    <section class="banner-content">
+        <h2>-20%</h2>
+        <p>su profumi e <br>skincare</p>
+        <a href="#" class="banner-btn">Scopri di più</a>
+    </section>
+</div>
+</html>
