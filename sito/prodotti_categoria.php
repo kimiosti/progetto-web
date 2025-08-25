2501424d7ead424031c4e01b528b4e7d841f7cb8
Giulia <giulia.albertini3@studio.unibo.it>
2025-08-25 17:52:32 +0200

    Modified products category content page to add availability without creating a duplicate of the product

--- sito/prodotti_categoria.php
+++ sito/prodotti_categoria.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'setup.php';
 
+if (!isset($_GET["categoria"]) || empty($_GET["categoria"])) {
+    header('Location: index.php');
+    exit();
+}
+
 $categoria = $_GET["categoria"];
 $marcaFiltro = $_GET["marca"] ?? [];
 $sottocategoriaFiltro = $_GET['sottocategoria'] ?? [];
@@ -13,45 +18,15 @@ $subcategoryList = $dbh->getSubcategory($categoria);
 $sizeList = $dbh->getSize($categoria);
 $priceList = $dbh->getPrice($categoria);
 
-
-if (!empty($search)) {
-    $filtroDaAggiungere = false;
-    $parametriURL = $_GET;
-
-    foreach ($brandList as $brand) {
-        if (strcasecmp($brand['marca'], $search) == 0 && !in_array($brand['marca'], $marcaFiltro)) {
-            $parametriURL['marca'][] = $brand['marca'];
-            $filtroDaAggiungere = true;
-            break;
-        }
-    }
-
-    if (!$filtroDaAggiungere) {
-        foreach ($subcategoryList as $sub) {
-            if (strcasecmp($sub['sottocategoria'], $search) == 0 && !in_array($sub['sottocategoria'], $sottocategoriaFiltro)) {
-                $parametriURL['sottocategoria'][] = $sub['sottocategoria'];
-                $filtroDaAggiungere = true;
-                break;
-            }
-        }
-    }
-
-    if ($filtroDaAggiungere) {
-        header('Location: prodotti_Categoria.php?' . http_build_query($parametriURL));
-        exit();
-    }
-}
-
-
 $templateParams["titolo"] = "PureEssence - Prodotti $categoria";
 $templateParams["categorie"] = $dbh->getCategories();
 $templateParams["descrizioneCategoria"] = $dbh->getDescriptionByCategories($categoria);
+
 
 $templateParams["prodotti"] = $dbh->getProductByCategories($categoria, $marcaFiltro, $sottocategoriaFiltro, $tagliaFiltro, $prezzoFiltro, $search);
 
 $templateParams["categoriaSelezionata"] = $categoria;
 $templateParams["main"] = "prodotti_categoria_contenuto.php";
-$templateParams["css"] = "style/prodotti_categoria.css";
 $templateParams["marcafiltri"] = $brandList;
 $templateParams["sottocategoriafiltri"] = $subcategoryList;
 $templateParams["taglieprodottofiltri"] = $sizeList;
--- sito/templates/prodotti_categoria_contenuto.php
+++ sito/templates/prodotti_categoria_contenuto.php
@@ -1,3 +1,31 @@
+<?php
+function generateFilterLinks($filterParamName, $availableOptions, $optionKey) {
+    $selectedOptions = $_GET[$filterParamName] ?? [];
+
+    foreach ($availableOptions as $option) {
+        $currentOptionValue = $option[$optionKey];
+        $params = $_GET;
+        $isSelected = in_array($currentOptionValue, $selectedOptions);
+
+        $tempOptions = $selectedOptions;
+        if ($isSelected) {
+            $tempOptions = array_diff($tempOptions, [$currentOptionValue]);
+        } else {
+            $tempOptions[] = $currentOptionValue;
+        }
+
+        $params[$filterParamName] = $tempOptions;
+        if (empty($params[$filterParamName])) {
+            unset($params[$filterParamName]);
+        }
+
+        $url = 'prodotti_Categoria.php?' . http_build_query($params);
+        $activeClass = $isSelected ? 'filtro-attivo' : '';
+
+        echo '<li><a href="' . htmlspecialchars($url) . '" class="' . htmlspecialchars($activeClass) . '">' . ucfirst(htmlspecialchars($currentOptionValue)) . '</a></li>';
+    }
+}
+?>
 
 <link rel="stylesheet" type="text/css" href="style/stylepage.css"/>
 <link rel="stylesheet" type="text/css" href="style/stylefilter.css"/>
@@ -25,12 +53,12 @@
             <h3 class="titoletto">Filtri</h3>
         </section>
 
-        <form class="form">
+        <form class="form" method="GET">
             <input type="hidden" name="categoria" value="<?php echo htmlspecialchars($templateParams['categoriaSelezionata']); ?>">
             <button type="submit">
                 <img class="decorazione" src="img/search-icon.svg" alt="Cerca">
             </button>
-            <input class="input" name="search" placeholder="Cerca prodotti..." required type="text" value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
+            <input class="input" name="search" placeholder="Cerca prodotti..." type="text" value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
             <button class="reset" type="button" onclick="location.href='prodotti_Categoria.php?categoria=<?php echo urlencode($templateParams['categoriaSelezionata']); ?>'">
                 <img class="decor" src="img/reset-icon.svg">
             </button>
@@ -39,102 +67,48 @@
 
     <section class="filtri">
         <section class="wrapper">
-            <button class="click">Marca <img class="arrow" src="img/arrow-up.png"></button>
+            <button class="click">Marca <img class="arrow" src="img/arrow-up.png" alt=""></button>
             <ul class="dropdown">
-                <?php
-                $marcheSelezionate = $_GET['marca'] ?? [];
-                foreach($templateParams['marcafiltri'] as $marca):
-                    $params = $_GET;
-                    $marcaCorrente = $marca['marca'];
-                    $isSelezionata = in_array($marcaCorrente, $marcheSelezionate);
-                    $marcheTemp = $marcheSelezionate;
-                    if ($isSelezionata) {
-                        $marcheTemp = array_diff($marcheTemp, [$marcaCorrente]);
-                    } else {
-                        $marcheTemp[] = $marcaCorrente;
-                    }
-                    $params['marca'] = $marcheTemp;
-                    if(empty($params['marca'])) { unset($params['marca']); }
-                    $url = 'prodotti_Categoria.php?' . http_build_query($params);
-                    $classeAttiva = $isSelezionata ? 'filtro-attivo' : '';
-                ?>
-                    <li><a href="<?php echo $url; ?>" class="<?php echo $classeAttiva; ?>"><?php echo ucfirst(htmlspecialchars($marcaCorrente)); ?></a></li>
-                <?php endforeach; ?>
+                <?php generateFilterLinks('marca', $templateParams['marcafiltri'], 'marca'); ?>
             </ul>
         </section>
 
         <section class="wrapper">
-            <button class="click">Tipologia di prodotto <img class="arrow" src="img/arrow-up.png"></button>
+            <button class="click">Tipologia di prodotto <img class="arrow" src="img/arrow-up.png" alt=""></button>
             <ul class="dropdown">
-                <?php
-                $sottocategorieSelezionate = $_GET['sottocategoria'] ?? [];
-                foreach($templateParams['sottocategoriafiltri'] as $sottocategoria):
-                    $params = $_GET;
-                    $sottocategoriaCorrente = $sottocategoria['sottocategoria'];
-                    $isSelezionata = in_array($sottocategoriaCorrente, $sottocategorieSelezionate);
-                    $sottocategorieTemp = $sottocategorieSelezionate;
-                    if ($isSelezionata) {
-                        $sottocategorieTemp = array_diff($sottocategorieTemp, [$sottocategoriaCorrente]);
-                    } else {
-                        $sottocategorieTemp[] = $sottocategoriaCorrente;
-                    }
-                    $params['sottocategoria'] = $sottocategorieTemp;
-                    if(empty($params['sottocategoria'])) { unset($params['sottocategoria']); }
-                    $url = 'prodotti_Categoria.php?' . http_build_query($params);
-                    $classeAttiva = $isSelezionata ? 'filtro-attivo' : '';
-                ?>
-                    <li><a href="<?php echo $url; ?>" class="<?php echo $classeAttiva; ?>"><?php echo ucfirst(htmlspecialchars($sottocategoriaCorrente)); ?></a></li>
-                <?php endforeach; ?>
+                <?php generateFilterLinks('sottocategoria', $templateParams['sottocategoriafiltri'], 'sottocategoria'); ?>
             </ul>
         </section>
 
         <section class="wrapper">
-            <button class="click">Taglia <img class="arrow" src="img/arrow-up.png"></button>
+            <button class="click">Taglia <img class="arrow" src="img/arrow-up.png" alt=""></button>
             <ul class="dropdown">
-                <?php
-                $taglieSelezionate = $_GET['taglia'] ?? [];
-                foreach($templateParams['taglieprodottofiltri'] as $taglia):
-                    $params = $_GET;
-                    $tagliaCorrente = $taglia['taglia'];
-                    $isSelezionata = in_array($tagliaCorrente, $taglieSelezionate);
-                    $taglieTemp = $taglieSelezionate;
-                    if ($isSelezionata) {
-                        $taglieTemp = array_diff($taglieTemp, [$tagliaCorrente]);
-                    } else {
-                        $taglieTemp[] = $tagliaCorrente;
-                    }
-                    $params['taglia'] = $taglieTemp;
-                    if(empty($params['taglia'])) { unset($params['taglia']); }
-                    $url = 'prodotti_Categoria.php?' . http_build_query($params);
-                    $classeAttiva = $isSelezionata ? 'filtro-attivo' : '';
-                ?>
-                    <li><a href="<?php echo $url; ?>" class="<?php echo $classeAttiva; ?>"><?php echo htmlspecialchars($tagliaCorrente); ?></a></li>
-                <?php endforeach; ?>
+                <?php generateFilterLinks('taglia', $templateParams['taglieprodottofiltri'], 'taglia'); ?>
             </ul>
         </section>
 
         <section class="wrapper">
-            <button class="click">Prezzo <img class="arrow" src="img/arrow-up.png"></button>
+            <button class="click">Prezzo <img class="arrow" src="img/arrow-up.png" alt=""></button>
             <ul class="dropdown">
                 <?php
-                $prezziSelezionati = $_GET['prezzo'] ?? [];
-                foreach($templateParams['prezzoprodottofiltri'] as $prezzo):
-                    $params = $_GET;
-                    $prezzoCorrente = $prezzo['prezzo'];
-                    $isSelezionato = in_array($prezzoCorrente, $prezziSelezionati);
-                    $prezziTemp = $prezziSelezionati;
-                    if ($isSelezionato) {
-                        $prezziTemp = array_diff($prezziTemp, [$prezzoCorrente]);
-                    } else {
-                        $prezziTemp[] = $prezzoCorrente;
+                    $prezziSelezionati = $_GET['prezzo'] ?? [];
+                    foreach($templateParams['prezzoprodottofiltri'] as $prezzo) {
+                        $prezzoCorrente = $prezzo['prezzo'];
+                        $params = $_GET;
+                        $isSelezionato = in_array($prezzoCorrente, $prezziSelezionati);
+                        $prezziTemp = $prezziSelezionati;
+                        if ($isSelezionato) {
+                            $prezziTemp = array_diff($prezziTemp, [$prezzoCorrente]);
+                        } else {
+                            $prezziTemp[] = $prezzoCorrente;
+                        }
+                        $params['prezzo'] = $prezziTemp;
+                        if(empty($params['prezzo'])) { unset($params['prezzo']); }
+                        $url = 'prodotti_Categoria.php?' . http_build_query($params);
+                        $classeAttiva = $isSelezionato ? 'filtro-attivo' : '';
+                        echo '<li><a href="' . htmlspecialchars($url) . '" class="' . htmlspecialchars($classeAttiva) . '">' . htmlspecialchars($prezzoCorrente) . ' €</a></li>';
                     }
-                    $params['prezzo'] = $prezziTemp;
-                    if(empty($params['prezzo'])) { unset($params['prezzo']); }
-                    $url = 'prodotti_Categoria.php?' . http_build_query($params);
-                    $classeAttiva = $isSelezionato ? 'filtro-attivo' : '';
                 ?>
-                    <li><a href="<?php echo $url; ?>" class="<?php echo $classeAttiva; ?>"><?php echo htmlspecialchars($prezzoCorrente); ?> €</a></li>
-                <?php endforeach; ?>
             </ul>
         </section>
     </section>
@@ -151,18 +125,15 @@
             if (is_array($valore)) {
                 foreach ($valore as $singoloValore) {
                     $params = $_GET;
-
                     $params[$chiave] = array_diff($params[$chiave], [$singoloValore]);
                     if (empty($params[$chiave])) {
                         unset($params[$chiave]);
                     }
-
                     if (isset($params['search']) && strcasecmp($params['search'], $singoloValore) == 0) {
                         unset($params['search']);
                     }
-
                     $url = 'prodotti_Categoria.php?' . http_build_query($params);
-                    echo '<span class="filtro-pill">' . htmlspecialchars($singoloValore) . '<a class="remove-filtro" href="' . $url . '">×</a></span>';
+                    echo '<span class="filtro-pill">' . htmlspecialchars($singoloValore) . '<a class="remove-filtro" href="' . htmlspecialchars($url) . '">×</a></span>';
                 }
             }
         }
@@ -171,30 +142,45 @@
     </section>
 
     <section class="elenco_prodotti">
-        <?php
-        if (empty($templateParams["prodotti"])) {
-            echo "<p>Nessun prodotto trovato.</p>";
-        } else {
-            foreach ($templateParams["prodotti"] as $prodotto) {
-                $link = isset($_SESSION["tipoUtente"]) && $_SESSION["tipoUtente"] == "venditore" ? "handle_availability.php" : "product_detail.php";
-                echo '<a href="' . $link . '?id=' . $prodotto['IDprodotto'] . (isset($_SESSION["tipoUtente"]) && $_SESSION["tipoUtente"] == "venditore" ? ('&categoria=' . $templateParams["categoriaSelezionata"]) : '') . '" class="prodotto-link">';
+    <?php
+    if (empty($templateParams["prodotti"])) {
+        echo "<p>Nessun prodotto trovato che soddisfi i criteri di ricerca.</p>";
+    } else {
+        foreach ($templateParams["prodotti"] as $prodotto) {
+            $link = (isset($_SESSION["tipoUtente"]) && $_SESSION["tipoUtente"] == "venditore") ? "handle_availability.php" : "product_detail.php";
+            $queryParams = [ 'id' => $prodotto['IDprodotto'] ];
+    ?>
+            <a href="<?php echo $link . '?' . http_build_query($queryParams); ?>" class="prodotto-link">
+                <div class="prodotto">
+                    <h3><?php echo htmlspecialchars(ucfirst($prodotto['marca'])); ?></h3>
+                    <img src="img/<?php echo htmlspecialchars($prodotto['URLimmagine']); ?>" alt="<?php echo htmlspecialchars($prodotto['nome']); ?>" />
+                    <h4><?php echo htmlspecialchars(ucfirst($prodotto['nome'])); ?></h4>
+                    <p><?php echo htmlspecialchars($prodotto['didascalia']); ?></p>
 
-                echo "<div class='prodotto'>";
-                echo "<h3>" . htmlspecialchars($prodotto['marca']) . "</h3>";
-                echo "<img src='img/" . htmlspecialchars($prodotto['URLimmagine']) . "' alt='" . htmlspecialchars($prodotto['nome']) . "' />";
-                echo "<h3>" . htmlspecialchars($prodotto['nome']) . "</h3>";
-                echo "<p>" . htmlspecialchars($prodotto['didascalia']) . "</p>";
+                    <?php if (!isset($_SESSION["tipoUtente"]) || $_SESSION["tipoUtente"] != "venditore"): ?>
+                        <div class="prodotto-bottom">
 
-                if (!isset($_SESSION["tipoUtente"]) || $_SESSION["tipoUtente"] != "venditore") {
-                    echo "<div class='prodotto-bottom'>";
-                    echo "<p class='prezzo'>€ " . number_format($prodotto['prezzo'], 2, ',', '') . "</p>";
-                    echo "<p class='taglia'>" . htmlspecialchars($prodotto['taglia']) . "</p>";
-                    echo "</div>";
-                }
-                echo "</div>";
-                echo '</a>';
-            }
+                            <p class="prezzo">
+                                <?php if(isset($prodotto['prezzo'])): ?>
+                                    € <?php echo number_format($prodotto['prezzo'], 2, ',', '.'); ?>
+                                <?php else: ?>
+                                    Non disponibile
+                                <?php endif; ?>
+                            </p>
+
+                            <?php if(isset($prodotto['taglia'])): ?>
+                                <p class="taglia">
+                                    <?php echo htmlspecialchars($prodotto['taglia']); ?>
+                                </p>
+                            <?php endif; ?>
+
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </a>
+    <?php
         }
-        ?>
-    </section>
+    }
+    ?>
+</section>
 </div>
