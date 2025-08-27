f319fb0b0689d2220f3c8aff96d5d27a6c4dbde5
Kimi Osti <kimi.osti@studio.unibo.it>
2025-08-27 10:41:53 +0200

    Modified product availabilities list template to match modified parameters

--- sito/templates/product_availabilities.php
+++ sito/templates/product_availabilities.php
@@ -2,9 +2,9 @@
 
 <section>
     <nav><a href="index.php">Home</a><span>/</span><a href="profile.php">Pagina personale</a><span>/</span><a href="availability.php">Gestione disponibilità</a><span>/</span><?php
-    if ($templateParams["prodotto"] != null) {
-        echo '<a href="prodotti_categoria.php?categoria=' . $templateParams["categoria"] . '">'
-            . ucfirst($templateParams["categoria"]) . '</a>';
+    if ($templateParams["categoria"] != null) {
+        echo '<a href="prodotti_categoria.php?categoria=' . $templateParams["categoria"]["nome"] . '">'
+            . ucfirst($templateParams["categoria"]["nome"]) . '</a>';
     }
     ?><span>/Singolo prodotto</span></nav>
 
@@ -21,9 +21,9 @@
         <header><h2><?php echo $templateParams["prodotto"]["marca"] . ' ' . $templateParams["prodotto"]["nome"] . ' - ' . htmlspecialchars($availability["taglia"]); ?></h2></header>
         <p>Disponibilità rimasta: <?php echo $availability["quantità"] ?></p>
         <p>Prezzo applicato: €<?php echo number_format($availability['prezzo'], 2, ',', '') ?></p>
-        <section><a href="modify_availability.php?id=<?php echo $availability["IDdisponibilità"]; ?>&categoria=<?php echo $_GET["categoria"]; ?>"><button>Modifica disponibilità</button></a></section>
+        <section><a href="modify_availability.php?id=<?php echo $availability["IDdisponibilità"]; ?>&categoria=<?php echo $templateParams["categoria"]["nome"]; ?>"><button>Modifica disponibilità</button></a></section>
         </div>
     <?php endforeach; ?>
 
-    <a href="add_availability.php?id=<?php echo $_GET["id"]; ?>&categoria=<?php echo $_GET["categoria"] ?>"><button>Aggiungi disponibilità</button></a>
+    <a href="add_availability.php?id=<?php echo $_GET["id"]; ?>&categoria=<?php echo $templateParams["categoria"]["nome"] ?>"><button>Aggiungi disponibilità</button></a>
 </section>
